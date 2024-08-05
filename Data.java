58de75044df55011a66ac31f13ed1c0a124b73a8
ewraan septian nur R <erwan_ramadhan_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:38:10 +0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,13 @@
+public class Data {
+    public static void main(String[] args) {
+        String nama = "R-1";
+        int usia = 16;
+        String alamat = "ngunut";
+        double kelas = 10;
+
+        System.out.println("my name is " + nama);
+        System.out.println("usia saya adalah " + usia +" tahun");
+        System.out.println("alamat saya berada di " + alamat);
+        System.out.println("saya sekarang berada di kelas " + kelas);
+    }
+}
